1d594fe58d63cb3d37e25e50d9b825844abd130d
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2025-07-20 17:31:50 -0500

    Agrega inicializacion del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./presentation/styles/index.css";
+function App() {
+  return (
+    <div className='flex flex-row min-h-screen justify-center bg-blue-procoinec'>
+      <h1 className='text-9xl'>
+        Skibidi Papu
+      </h1>
+      <div className="bg-orange-procoinec">
+        <p>Papu</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
